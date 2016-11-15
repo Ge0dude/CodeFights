b1d4d10ffc3edc6436b8fc817862ca7e316bbcf3
Brendon <btucker1992@gmail.com>
2016-11-15 13:42:19 +0800

    forgot to add... MetroCard was really easy?

--- Metro Card.py
+++ Metro Card.py
@@ -5,3 +5,10 @@ Created on Tue Nov 15 13:39:32 2016
 @author: brendontucker
 """
 
+def metroCard(lastNumberOfDays):
+    if lastNumberOfDays == 31:
+        return [28, 30, 31]
+    else:
+        return [31]
+
+
