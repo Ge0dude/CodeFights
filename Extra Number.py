53f8f985dad89fb58aae44a025e6c3d64227e82a
Brendon <btucker1992@gmail.com>
2016-11-15 09:08:50 +0800

    difficulty on these is weird

---
+++ Extra Number.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 09:05:27 2016
+
+@author: brendontucker
+"""
+
+def extraNumber(a, b, c):
+    if a == b:
+        return c
+    if a == c:
+        return b
+    else: 
+        return a
+        
