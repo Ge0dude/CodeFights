021c59e9868d0a40b0ead59ce5eaea0f7447a0e9
Brendon <btucker1992@gmail.com>
2016-11-17 14:32:58 +0800

    need to also consider negatives in Is Smooth?

---
+++ Is Smooth? .py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 12:41:13 2016
+
+@author: brendontucker
+"""
+
+boo1 = False
+boo2 = False
+boo3 = False
+
+
+array1 = [-12, 34, 40, -5, -12, 4, 0, 0, -12]
+if len(array1) % 2 == 0:
+    length1 = len(array1) / 2
+    length2 = int(length1)
+    index1 = array1[length2 - 1]
+    index3 = array1[-length2]
+    ans1 = (array1[length2 - 1] + array1[-length2])
+    if ans1 == array1[0]:
+        boo1 = True
+    if ans1 == array1[-1]:
+        boo2 = True
+    if boo1 == True and boo2 == True:
+        boo3 = True
+
+if len(array1) % 2 != 0:
+    length3 = (len(array1) // 2) + 1
+    if array1[length3] == array1[0]:
+        boo1 = True
+    if array1[length3] == array1[-1]:
+        boo2 = True
+    if boo1 == True and boo2 == True:
+        boo3 = True   
+    
+    
+    
+    
+    
+    
+
