073721452a5815d54f8083faf0c2e0f95b2210bc
Brendon <btucker1992@gmail.com>
2016-11-17 09:54:39 +0800

    Taking a break from Lineup... can't find loop error

--- Lineup.py
+++ Lineup.py
@@ -5,7 +5,7 @@ Created on Wed Nov 16 10:14:21 2016
 @author: brendontucker
 """
 
-commands = 'AAAAA'
+commands = 'ALLALLLL'
 
 currPos = 0
 poslist = list()
@@ -21,6 +21,18 @@ while posInList <= length1 - 1:
     if posCounter == 'L' and currPos == 0:
         poslist.insert(len(poslist), 3)
         currPos = 3
+        posInList = posInList + 1
+    if posCounter == 'L' and currPos == 1:
+        poslist.insert(len(poslist), 0)
+        currPos = 0
+        posInList = posInList + 1
+    if posCounter == 'L' and currPos == 2:
+        poslist.insert(len(poslist), 1)
+        currPos = 1
+        posInList = posInList + 1
+    if posCounter == 'L' and currPos == 3:
+        poslist.insert(len(poslist), 2)
+        currPos = 2
         posInList = posInList + 1
         
     
