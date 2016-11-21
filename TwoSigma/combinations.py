e814bd4052c0da73804f94984afb794ec1d47e95
Brendon <btucker1992@gmail.com>
2016-11-21 10:13:45 +0800

    don't know enough about iterables... very difficult right now

--- TwoSigma/combinations.py
+++ TwoSigma/combinations.py
@@ -7,33 +7,12 @@ Created on Sun Nov 20 17:02:54 2016
 
 import itertools
 
+stocks = [[-1,2], [10,15], [11,13], [9,10]]
+length = len(stocks)
 iterable = range(4)
 r = 4
 
 
 while r > 0:
-    for x in itertools.combinations(iterable, r):
-        print (x)
+    list(itertools.combinations(stocks, r))
     r = r - 1
-'''
-def combinations(iterable, r):
-    # combinations('ABCD', 2) --> AB AC AD BC BD CD
-    # combinations(range(4), 3) --> 012 013 023 123
-    pool = tuple(iterable)
-    n = len(pool)
-    if r > n:
-        return
-    indices = list(range(r))
-    yield tuple(pool[i] for i in indices)
-    while True:
-        for i in reversed(range(r)):
-            if indices[i] != i + n - r:
-                break
-        else:
-            return
-        indices[i] += 1
-        for j in range(i+1, r):
-            indices[j] = indices[j-1] + 1
-        yield tuple(pool[i] for i in indices)
-
-'''     
