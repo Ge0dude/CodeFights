949ea0cb2f5d391b28ce1ff11e003df04b0c15f7
Brendon <btucker1992@gmail.com>
2016-11-24 10:52:20 +0800

    combos are up to date now

--- TwoSigma/combinations.py
+++ TwoSigma/combinations.py
@@ -10,10 +10,15 @@ import itertools
 stocks = [[-1,2], [10,15], [11,13], [9,10]]
 length = len(stocks)
 iterable = range(4)
-r = 4
+r = length 
 
 x = 0
+group = list()
 
 while r > 0:
-    group1 = list(itertools.combinations(stocks, r))
+    list1 = list(itertools.combinations(stocks, r))
+    group.append(list1)
     r = r - 1
+    
+
+
