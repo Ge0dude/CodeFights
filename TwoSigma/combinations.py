89fd1e60bd960c897497edd4773e02467b71fadd
Brendon <btucker1992@gmail.com>
2016-11-29 13:01:34 +0800

    making progress, very diff.

--- TwoSigma/combinations.py
+++ TwoSigma/combinations.py
@@ -11,6 +11,12 @@ stocks = [[-1,2], [10,15], [11,13], [9,10]]
 length = len(stocks)
 iterable = range(4)
 r = length 
+valuesList = list()
+riskList = list()
+lengthL = 0
+optionL = 0
+tradeL = 0
+binaryL = 0 
 
 x = 0
 group = list()
@@ -19,6 +25,17 @@ while r > 0:
     list1 = list(itertools.combinations(stocks, r))
     group.append(list1)
     r = r - 1
+
+
+while lengthL < len(group):
+    while optionL < len(group[lengthL][optionL]):
+        while tradeL < len(group[lengthL][optionL][tradeL]):
+            valueSum = 
+            valuesList.append(group[lengthL][optionL][tradeL][0])
+            tradeL = tradeL + 1
+        optionL = optionL + 1
+    lengthL = lengthL + 1
+        
     
 
 
