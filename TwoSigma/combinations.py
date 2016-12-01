62be066b7e60ad891cee1beb27ecadc6f2c2e98a
Brendon <btucker1992@gmail.com>
2016-12-01 13:03:29 +0800

    got the first four correct! progress!!

--- TwoSigma/combinations.py
+++ TwoSigma/combinations.py
@@ -28,20 +28,38 @@ while r > 0:
 
 valuesSum = 0
 
+for x in group:
+    for x in group[lengthL][optionL]:
+        stockValue = group[lengthL][optionL][tradeL][0]
+        valuesSum = valuesSum + stockValue
+        tradeL = tradeL + 1
+        if tradeL == len(group[lengthL][optionL]):
+            valuesList.append(valuesSum)
+            valuesSum = 0
+    if optionL < (len(group[lengthL]) -1):
+        optionL = optionL + 1
+        tradeL = 0
+    else:
+        lengthL = lengthL + 1
+        optionL = 0
+        tradeL = 0
 
+
+'''
 #I think this one is really close, just need to alter
 #where the valuesSum is happening, need to go back to inner loop
 while lengthL <= (len(group) - 1):
-    while optionL <= (len(group[0]) - 1):
+    while optionL <= (len(group[lengthL]) - 1):
         for x in group[lengthL][optionL]:
             stockValue = group[lengthL][optionL][tradeL][0]
             valuesSum = valuesSum + stockValue
             tradeL = tradeL + 1
-        valuesList.append(valuesSum)
-        valuesSum = 0
+            if tradeL == (len(group[lengthL][tradeL]) - 1):
+                valuesList.append(valuesSum)
+                valuesSum = 0
         optionL = optionL + 1
     lengthL = lengthL + 1
-
+'''
 
 '''
 for x in group:
