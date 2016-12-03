829aa186189eb49108dfb3e01ff358cb2133b273
Brendon <btucker1992@gmail.com>
2016-12-03 11:17:27 +0800

    that took forever but i got it!!

--- New Road System.py
+++ New Road System.py
@@ -13,23 +13,56 @@ Created on Thu Nov 24 11:30:27 2016
 false = 0
 true = 1
 
-roadRegister =  [[false,true,false,false,false,false,false], 
-                 [true,false,false,false,false,false,false], 
-                 [false,false,false,true,false,false,false], 
-                 [false,false,true,false,false,false,false], 
-                 [false,false,false,false,false,false,true], 
-                 [false,false,false,false,true,false,false], 
-                 [false,false,false,false,false,true,false]]
+roadRegister =  [[false,true,true,true,true], 
+                 [true,false,true,true,true], 
+                 [true,true,false,true,true], 
+                 [true,true,true,false,true], 
+                 [true,true,true,true,false]]
 
-list1 = list()                
+length = len(roadRegister)
 
 rowNum = 0
 colNum = 0
-#knowlege from two sigma should help me be able to do this now--better 
-#understanding of loops
+rowCount = 0
+colCount = 0
+
+boo1 = False
 r = (len(roadRegister) - 1)
-#something about special case if row and col num have been found needed
-#in first if block
+
+while r >= 0:
+    rowNum = r
+    colNum = 0
+    rowCount = 0
+    colCount = 0
+    while colNum < len(roadRegister):
+        if roadRegister[rowNum][colNum] == 1:
+            rowCount = rowCount + 1
+            colNum = colNum + 1
+        else:
+            colNum = colNum + 1
+    colNum = r
+    rowNum = 0
+    while rowNum < len(roadRegister):
+        if roadRegister[rowNum][colNum] == 1:
+            colCount = colCount + 1
+            rowNum = rowNum + 1
+        else:
+            rowNum = rowNum + 1
+    if r == 0:
+        if rowCount == colCount:
+            boo1 = True
+            break
+    elif rowCount == colCount:
+        r = r - 1 
+    else:
+        break
+
+#return boo1
+    
+
+
+
+'''
 while r >= 0:
     rowNum = r
     if roadRegister[rowNum][colNum] == 1:
@@ -54,7 +87,7 @@ if len(list1) == len(roadRegister):
 else:
     ans = False
 
-            
+'''            
             
 
 
